da4559eb8580f5172ba9973402b25454537294b4
Jillella Likitha Reddy (Omnicom) <jillella.likithareddy@omc.com>
2026-09-02 11:19:05 +0530

    Fix order customer validation

--- lambda/order/lambda_function.py
+++ lambda/order/lambda_function.py
@@ -218,7 +218,7 @@ def validate_customer(
             user_id,
             name,
             email,
-            role,
+            role
         FROM users
         WHERE user_id = %s
         """,
